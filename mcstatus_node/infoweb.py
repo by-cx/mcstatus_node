26e4c349d4e91469cd132ecf3ef033754b562c52
Adam Štrauch <cx@initd.cz>
2013-03-27 16:31:28 +0100

    Init

---
+++ mcstatus_node/infoweb.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+
+import sys
+import json
+import shlex
+from bottle import route, run, template, response, abort, default_app
+from subprocess import PIPE, Popen
+
+def run_cmd(cmd):
+    p = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    return stdout
+
+@route('/')
+def info():
+    response.content_type = 'text/json'
+    memory = get_memory()
+    cpu = get_cpus()
+    load = get_load()
+    
+    data = {
+        "memory": memory["memory"],
+        "memory_max": memory["memory_max"],
+        "cpu_count": cpu["cpu_count"],
+        "load1": load[0],
+        "load5": load[1],
+        "load15": load[2],
+        "port": get_port(),
+    }
+    return json.dumps(data, indent=4)
+
+def get_port():
+    port = 0
+    try:
+        with open("/home/bukkit/server.properties") as f:
+            for line in f.readlines():
+                if "server-port" in line:
+                    port = int(line.strip().split("=")[1])
+    except IOError:
+        pass
+    return port
+
+def get_load():
+    data = (0.0, 0.0, 0.0)
+    try:
+        output = run_cmd("uptime").strip()
+    except IOError:
+        pass
+    if len(output.split(" ")) > 3:
+        load1m = float(output.split(" ")[-1].strip(" ,").replace(",", "."))
+        load5m = float(output.split(" ")[-2].strip(" ,").replace(",", "."))
+        load15m = float(output.split(" ")[-3].strip(" ,").replace(",", "."))
+        data = (load1m, load5m, load15m)
+    return data
+
+def get_memory():
+    output = {"memory": 0, "memory_max": 0}
+    try:
+        data = [x.strip().split() for x in run_cmd("free -m").split("\n")]
+        output["memory_max"] = data[1][1]
+        output["memory"] = data[2][2]
+    except IOError:
+        pass
+    return output
+
+def get_cpus():
+    data = {"cpu_count": 1}
+    try:
+        output = run_cmd("cat /proc/cpuinfo").strip()
+    except IOError:
+        output = ""
+    if output:
+        cpus = []
+        for x in [x.strip() for x in output.split("\n")]:
+            if "processor" in x:
+                cpus.append(int(x.split()[2].strip()))
+        data["cpu_count"] = max(cpus)+1 if cpus else 1
+    return data
+
+def main():
+    run(host='193.150.12.34', port=5000)
+
+if __name__ == "__main__":
+     main()
+else:
+     application = default_app()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mcstatus_node",
+    version = "1.0",
+    author = "Adam Strauch",
+    author_email = "cx@initd.cz",
+    description = ("Node webapp for mcstatus"),
+    license = "BSD",
+    keywords = "minecraft",
+    url = "https://github.com/creckx/mcstatus_node",
+    long_description="Node webapp for mcstatus",
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        ],
+    entry_points="""
+    [console_scripts]
+    mcstatus_node = mcstatus_node.infoweb:main
+    """
+)
